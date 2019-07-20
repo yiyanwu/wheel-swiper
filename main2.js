166b565de670bbf9c67ddeff158562c34963c490
wuyiyan <308111394@qq.com>
2019-07-20 15:35:48 +0800

    用getter封装 有关于index的获取

--- main2.js
+++ main2.js
@@ -10,37 +10,41 @@ class Carousel {
         this.bind()
     }
 
+    get index (){
+        return this.dots.indexOf(this.dotsCt.querySelector('.active'))
+    }
+    get preIndex (){
+        return (this.index - 1 + this.panels.length) % this.panels.length
+    }
+    get nextIndex (){
+        return (this.index + 1) % this.panels.length
+    }
+
     bind() {
         this.dotsCt.onclick = (e) => {
             if (e.target.tagName !== 'SPAN') return
             let index = this.dots.indexOf(e.target)
-            this.setZIndex(index)
-            this.setActive(index)
+            this.setPanels(index)
+            this.setDots(index)
         }
 
         this.pre.onclick = (e) => {
-            let index = this.dots.indexOf(this.dotsCt.querySelector('.active'))
-            let preIndex = (index - 1 + this.panels.length) % this.panels.length
-
-            this.setZIndex(preIndex)
-            this.setActive(preIndex)
+            this.setPanels(this.preIndex)
+            this.setDots(this.preIndex)  //Dots会发生变化，所以先设置其他的，最后设置Dots
         }
 
         this.next.onclick = (e) => {
-            let index = this.dots.indexOf(this.dotsCt.querySelector('.active'))
-            let nextIndex = (index + 1) % this.panels.length
-
-            this.setZIndex(nextIndex)
-            this.setActive(nextIndex)
+            this.setPanels(this.nextIndex)
+            this.setDots(this.nextIndex)
         }
     }
 
-    setZIndex(index) {
+    setPanels(index) {
         this.panels.forEach(a => a.style.zIndex = 1)
         this.panels[index].style.zIndex = 10
     }
 
-    setActive(index) {
+    setDots(index) {
         this.dots.forEach(dot => dot.classList.remove('active'))
         this.dots[index].classList.add('active')
     }
